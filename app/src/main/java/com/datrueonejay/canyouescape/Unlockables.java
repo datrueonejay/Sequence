f5b77b04fb3379f8f0786c13b511cb3f0960da89
datrueonejay <Iamthabest1>
2017-02-02 10:37:38 -0500

    Almost Finished App

---
+++ app/src/main/java/com/datrueonejay/canyouescape/Unlockables.java
@@ -0,0 +1,13 @@
+package com.datrueonejay.canyouescape;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Unlockables extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unlockables);
+    }
+}
